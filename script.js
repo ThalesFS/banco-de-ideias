2c98dddd3f038b71105331fb9a243177c399d746
ThalesFS <thalesfs@id.uff.br>
2025-11-01 19:50:22 -0300

    Cadastro melhorado

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+fetch("http://localhost:8080/api/professores")
+.then(response => response.json())
+.then(data => {
+  console.log("Professores via API:", data);
+  const lista = document.getElementById("lista-professores");
+  if(lista){
+    lista.innerHTML = data.map(nome => `<li>${nome}</li>`).join("");
+  }
+})
+.catch(error => console.error("Erro ao buscar professores", error));
+
 const botaoExplorar = document.getElementById('btnExplorar');
 
 botaoExplorar.addEventListener('click', () => {
